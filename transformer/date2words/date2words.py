e25c0a4495fdd131594d576b3e4d123623b7410c
Amir79Naziri <naziriamirreza@gmail.com>
2021-08-07 12:59:36 +0430

    "feat: add randomness in date transfer"

--- transformer/date2words/date2words.py
+++ transformer/date2words/date2words.py
@@ -2,6 +2,7 @@ from transformer.num2words import num2words
 from typing import Union
 from functools import singledispatch
 import re
+import random
 
 NUM2MONTHS = {
     1: 'فروردین',
@@ -60,6 +61,21 @@ NUM2MONTHS = {
     '١٢': 'اسفند'
 }
 
+MONTHS2NUM = {
+    'فروردین': 1,
+    'اردیبهشت': 2,
+    'خرداد': 3,
+    'تیر': 4,
+    'مرداد': 5,
+    'شهریور': 6,
+    'مهر': 7,
+    'آبان': 8,
+    'آذر': 9,
+    'دی': 10,
+    'بهمن': 11,
+    'اسفند': 12
+}
+
 MONTHS = [
     'فروردین',
     'اردیبهشت',
@@ -87,12 +103,14 @@ MONTHS = [
 @singledispatch
 def words(
         date: Union[str, list],
+        random_result=False
 ) -> str:
     raise TypeError('invalid input type for words function', date)
 
 
 def find_date_std(
-        date: str, delimiter: tuple = ('/', '.', '-')
+        date: str, delimiter: tuple = ('/', '.', '-'),
+        random_result=False
 ) -> str:
     for de in delimiter:
         sp = date.split(de)
@@ -105,30 +123,60 @@ def find_date_std(
             year = num2words.words(match.group())
             try:
                 month = NUM2MONTHS[match2.group()]
+                number_month = num2words.words(match2.group())
             except KeyError:
                 break
             day = num2words.words(match3.group())
-            return day + ' ' + month + ' ' + 'سال ' + year
+            if not random_result:
+                return v1(day, month, year)
+            else:
+                rand = random.randint(1, 4)
+                if rand == 1:
+                    return v1(day, month, year)
+                elif rand == 2:
+                    return v2(day, month, year)
+                elif rand == 3:
+                    return v3(day, number_month, year)
+                else:
+                    return v4(day, number_month, year)
     raise TypeError('invalid input type for words function', date)
+
+
+def v1(day, month, year):
+    return day + ' ' + month + ' ' + 'سال ' + year
+
+
+def v2(day, month, year):
+    return day + ' ' + month + ' ' + year
+
+
+def v3(day, number_month, year):
+    return day + ' ' + number_month + ' ' + 'سال ' + year
+
+
+def v4(day, number_month, year):
+    return day + ' ' + number_month + ' ' + year
 
 
 @words.register(str)
 def _(
         date: str,
+        random_result=False
 ) -> str:
-    return find_date_std(date)
+    return find_date_std(date, random_result=random_result)
 
 
 @words.register(list)
 def _(
         date: list,
+        random_result=False
 ) -> str:
     length = len(date)
     if length == 1:
-        return find_date_std(''.join(date))
+        return find_date_std(''.join(date), random_result=random_result)
     if 5 >= length >= 2:
         counter = 0
-        output = ''
+        day, month, number_month = None, None, None
 
         for d in date:
 
@@ -137,20 +185,22 @@ def _(
                 if match is None:
                     continue
                 number = int(match.group())
-                output += num2words.words(number) + ' '
+                day = num2words.words(number)
                 counter += 1
 
             elif counter == 1:
                 match = re.search(r'\d+', d)
                 if match is None:
                     if d in MONTHS:
-                        output += d + ' '
+                        month = d
+                        number_month = num2words.words(MONTHS2NUM[month])
                     else:
                         continue
                 else:
                     number = int(match.group())
                     try:
-                        output += NUM2MONTHS[number] + ' '
+                        month = NUM2MONTHS[number]
+                        number_month = num2words.words(number)
                     except KeyError:
                         break
                 counter += 1
@@ -160,7 +210,19 @@ def _(
                 if match is None:
                     continue
                 number = int(match.group())
-                output += 'سال ' + num2words.words(number)
-                return output
+                year = num2words.words(number)
+
+                if not random_result:
+                    return v1(day, month, year)
+                else:
+                    rand = random.randint(1, 4)
+                    if rand == 1:
+                        return v1(day, month, year)
+                    elif rand == 2:
+                        return v2(day, month, year)
+                    elif rand == 3:
+                        return v3(day, number_month, year)
+                    else:
+                        return v4(day, number_month, year)
 
     raise TypeError('invalid input type for words function', date)
